cb147d3ae5a018e0c8558983eeed623ff80a2903
Ryan Cullum <racullum@gmail.com>
2019-01-23 18:17:38 -0600

    Updates

---
+++ precache-manifest.d5b9256dc2d40c3eed75173168d014af.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "324cdcf6133a38410dca",
+    "url": "/game-suggestion-lookup/static/css/main.4452a315.chunk.css"
+  },
+  {
+    "revision": "324cdcf6133a38410dca",
+    "url": "/game-suggestion-lookup/static/js/main.324cdcf6.chunk.js"
+  },
+  {
+    "revision": "532d8642f1e8c96e91a2",
+    "url": "/game-suggestion-lookup/static/css/1.faae105e.chunk.css"
+  },
+  {
+    "revision": "532d8642f1e8c96e91a2",
+    "url": "/game-suggestion-lookup/static/js/1.532d8642.chunk.js"
+  },
+  {
+    "revision": "60dbc4660a61a16660d6",
+    "url": "/game-suggestion-lookup/static/js/runtime~main.60dbc466.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/game-suggestion-lookup/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/game-suggestion-lookup/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/game-suggestion-lookup/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/game-suggestion-lookup/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/game-suggestion-lookup/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "8754c42debb2fd666919522b8fba249c",
+    "url": "/game-suggestion-lookup/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/game-suggestion-lookup/precache-manifest.d5b9256dc2d40c3eed75173168d014af.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/game-suggestion-lookup/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
