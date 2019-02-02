c75b0aedd43845537291632d86aca618f6f4036d
Ryan Cullum <racullum@gmail.com>
2019-02-02 03:03:02 -0600

    Getting rid of catch all route

--- server/app.js
+++ server/app.js
@@ -54,10 +54,10 @@ app.post('/api/covers', (req, res) => {
     });
 })
 
-// Handles any requests that don't match the ones above
+/*// Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
     res.sendFile(path.join(__dirname+'/../client/build/index.html'));
-});
+});*/
 
 const port = process.env.PORT || 3001;
 app.listen(port);
